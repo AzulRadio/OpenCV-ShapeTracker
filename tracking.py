df0ada0876e606019a28204fb11e9625b3242e81
AzulRadio <50132891+AzulRadio@users.noreply.github.com>
2020-09-13 01:11:10 +0800

    Add luminance balancing and noise generating module. (#1)
    * resistance to luminance imbalance in src
    
    * add noise generator
    
    * add noise generator

--- tracking.py
+++ tracking.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
-
+from luminance_balance import luminance_local_based
+from noise_generator import *
 def nothing(var):
     pass
 
@@ -11,52 +12,61 @@ _, temp = cap.read()
 cv2.imshow('frame', np.zeros_like(temp))
 
 cv2.createTrackbar('Threshold','frame',220, 255, nothing)
-
-center = np.zeros((2,0))
+cv2.createTrackbar('epsilon','frame',50, 100, nothing)
+cv2.createTrackbar('guassian_std','frame',25, 50, nothing)
+cv2.createTrackbar('sp_prob','frame',25, 100, nothing)
+prob, std = 100, 100
+#last = np.zeros(2)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
+    center = np.zeros(2)
     
-    # Our operations on the frame come here
+
+    # binay and median_blur
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    gray = sp_noise(gray, prob/1000)
+    gray = guassian_noise(gray, std = std/100)
+    gray = luminance_local_based(gray)
+    gray = cv2.GaussianBlur(gray, (101,101), 1)
+    cv2.imshow('gray',gray)
     thres = cv2.getTrackbarPos('Threshold','frame')
-    
-    _, binary = cv2.threshold(gray, thres, 255, cv2.THRESH_BINARY_INV)
+    epsilon_const = cv2.getTrackbarPos('epsilon','frame')
+    std = cv2.getTrackbarPos('guassian_std','frame')
+    prob = cv2.getTrackbarPos('sp_prob','frame')
+    _, output = cv2.threshold(gray, thres, 255, cv2.THRESH_BINARY_INV)
+    #binary_blur = cv2.medianBlur(output, 11)
+    #output = binary_blur
 
-    binary_blur = cv2.medianBlur(binary, 11)
-
-    output = binary_blur
-
+    #find contours
     contours, hierarchy = cv2.findContours(output,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+    
     for i in range(0,len(contours)):
         area = cv2.contourArea(contours[i])
         if area < 50 or area > 100000:
             continue
-        epsilon = 0.01 * cv2.arcLength(contours[i], True)
+        epsilon = epsilon_const/1000 * cv2.arcLength(contours[i], True)
         approx = cv2.approxPolyDP(contours[i], epsilon, True)
         corners = len(approx)
-        if corners > 8:
+        if corners > 16:
             continue
         #x, y, w, h = cv2.boundingRect(contours[i])
         rect = cv2.minAreaRect(contours[i])
         box = cv2.boxPoints(rect)
         box = np.int0(box)
-        output = cv2.drawContours(output,[box],0,(0,0,255),2)
+        output = cv2.drawContours(output,[box],0,(0,255,255),2)
+        frame = cv2.drawContours(frame,[box],0,(0,255,255),2)
         
         mm = cv2.moments(contours[i])
         cx = int(mm['m10'] / (mm['m00'] + 1e-6))
         cy = int(mm['m01'] / (mm['m00'] + 1e-6))
-        cv2.circle(output, (cx, cy), 3, (100, 255, 100), -1)
-        
-    # Display the resulting frame
+        center = np.vstack((center, np.array([cx, cy])))
+        cv2.circle(frame, (cx, cy), 3, (100, 255, 100), -1)
+    # Display the resulting frame1
     cv2.imshow('frame',frame)
     cv2.imshow('output', output)
-
-    #cv2.imshow('blur',gaussian)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
